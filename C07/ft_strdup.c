7b4c5704d1befd091385b95dc5867baf7cfc149a
July <july@student.42.fr>
2026-04-23 03:12:28 +0800

    ex00 submit

--- C07/ft_strdup.c
+++ C07/ft_strdup.c
@@ -1,16 +1,27 @@
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
 char	*ft_strdup(char *src)
 {
 	int		i;
+	int		len;
 	char	*dup;
 
-	i = 0;
-	while (src[i] != '\0')     // count length inline
-		i++;
-	dup = malloc(sizeof(char) * (i + 1));
+	len = ft_strlen(src);
+	dup = malloc(sizeof(char) * (len + 1));
 	if (dup == NULL)
 		return (NULL);
 	i = 0;
-	while (src[i] != '\0')     // walk again to copy
+	while (src[i] != '\0')
 	{
 		dup[i] = src[i];
 		i++;
