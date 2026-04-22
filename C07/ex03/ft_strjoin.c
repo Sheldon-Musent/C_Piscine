111258c960855e52050c2835249c2046ad989342
July <july@student.42.fr>
2026-04-23 03:36:25 +0800

    ex03 submit

---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*result;
+	int		total;
+	int		i;
+
+	if (size <= 0)
+	{
+		result = malloc(sizeof(char));
+		if (result == NULL)
+			return (NULL);
+		result[0] = '\0';
+		return (result);
+	}
+	total = 0;
+	i = 0;
+	while (i < size)
+	{
+		total += ft_strlen(strs[i]);
+		i++;
+	}
+	total += ft_strlen(sep) * (size - 1);
+	result = malloc(sizeof(char) * (total + 1));
+	if (result == NULL)
+		return (NULL);
+	result[0] = '\0';
+	i = 0;
+	while (i < size)
+	{
+		ft_strcat(result, strs[i]);
+		if (i < size - 1)
+			ft_strcat(result, sep);
+		i++;
+	}
+	return (result);
+}
