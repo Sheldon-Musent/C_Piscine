518170f59d7e8074c2e6d07ccaceae993fc10eed
July <july@student.42.fr>
2026-04-23 03:11:35 +0800

    ex00 test

---
+++ C07/ft_strdup.c
@@ -0,0 +1,55 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		len;
+	char	*dup;
+
+	len = ft_strlen(src);
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	char	*copy;
+
+	copy = ft_strdup("Hello World");
+	ft_putstr(copy);
+	write(1, "\n", 1);
+	free(copy);
+	return (0);
+}
