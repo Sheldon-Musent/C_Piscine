4e3c5e35174abc43e838cb20a1257c9671e049c7
July <july@student.42.fr>
2026-04-22 11:15:40 +0800

    C04,EX04

---
+++ C04/ex04/ft_putnbr_base.c
@@ -0,0 +1,74 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	is_valid_base(char *base)
+{
+	int	i;
+	int	j;
+
+	if (base[0] == '\0' || base[1] == '\0')
+		return (0);
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		if (base[i] <= 32 || base[i] == 127)
+			return (0);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	base_len;
+	long	n;
+
+	if (!is_valid_base(base))
+		return ;
+	base_len = ft_strlen(base);
+	n = nbr;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n >= base_len)
+		ft_putnbr_base(n / base_len, base);
+	ft_putchar(base[n % base_len]);
+}
+
+int	main(void)
+{
+	ft_putnbr_base(42, "0123456789");
+	write(1, "\n", 1);
+	ft_putnbr_base(42, "01");
+	write(1, "\n", 1);
+	ft_putnbr_base(42, "0123456789ABCDEF");
+	write(1, "\n", 1);
+	ft_putnbr_base(-42, "0123456789");
+	write(1, "\n", 1);
+	return (0);
+}
