7e11f9d71774ba9cac5aa37f913f585c7da28d42
July <july@student.42.fr>
2026-04-22 21:43:13 +0800

    ex00 testing version with main

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar('0' + nb % 10);
+}
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 0 || nb > 12)
+		return (0);
+	result = 1;
+	while (nb > 1)
+	{
+		result = result * nb;
+		nb--;
+	}
+	return (result);
+}
+
+int	main(void)
+{
+	ft_putnbr(ft_iterative_factorial(5));
+	write(1, "\n", 1);
+	ft_putnbr(ft_iterative_factorial(0));
+	write(1, "\n", 1);
+	ft_putnbr(ft_iterative_factorial(-3));
+	write(1, "\n", 1);
+	return (0);
+}
