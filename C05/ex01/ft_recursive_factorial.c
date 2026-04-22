ac7f5abf15fae707f1391d0798b051f89c081c06
July <july@student.42.fr>
2026-04-22 22:21:28 +0800

    ex01 testing version

---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar('0' + nb % 10);
+}
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb <= 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+int	main(void)
+{
+	ft_putnbr(ft_recursive_factorial(5));
+	write(1, "\n", 1);
+	ft_putnbr(ft_recursive_factorial(0));
+	write(1, "\n", 1);
+	ft_putnbr(ft_recursive_factorial(-3));
+	write(1, "\n", 1);
+	return (0);
+}
