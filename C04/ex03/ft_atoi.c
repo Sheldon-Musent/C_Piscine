7c1557be5d7197ea3fe2196c03554d314d75b35e
July <july@student.42.fr>
2026-04-22 11:11:32 +0800

    C04,EX03

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,61 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar('0' + nb % 10);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+int	main(void)
+{
+	ft_putnbr(ft_atoi("   42"));
+	write(1, "\n", 1);
+	ft_putnbr(ft_atoi("-123"));
+	write(1, "\n", 1);
+	ft_putnbr(ft_atoi("   ++-42abc"));
+	write(1, "\n", 1);
+	ft_putnbr(ft_atoi("hello"));
+	write(1, "\n", 1);
+	return (0);
+}
