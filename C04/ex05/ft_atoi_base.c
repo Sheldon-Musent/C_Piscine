17fd6eadd80ed87bf2ad627b7e473f4ed57480c2
July <july@student.42.fr>
2026-04-23 03:53:48 +0800

    add C04 ex05

---
+++ C04/ex05/ft_atoi_base.c
@@ -0,0 +1,114 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar('0' + nb % 10);
+}
+
+int	is_valid_base(char *base)
+{
+	int	i;
+	int	j;
+
+	if (base[0] == '\0' || base[1] == '\0')
+		return (0);
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		if (base[i] <= 32 || base[i] == 127)
+			return (0);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	base_len(char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	char_in_base(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	i;
+	int	sign;
+	int	result;
+	int	digit;
+
+	if (!is_valid_base(base))
+		return (0);
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while ((digit = char_in_base(str[i], base)) != -1)
+	{
+		result = result * base_len(base) + digit;
+		i++;
+	}
+	return (result * sign);
+}
+
+int	main(void)
+{
+	ft_putnbr(ft_atoi_base("42", "0123456789"));
+	write(1, "\n", 1);
+	ft_putnbr(ft_atoi_base("101010", "01"));
+	write(1, "\n", 1);
+	ft_putnbr(ft_atoi_base("2A", "0123456789ABCDEF"));
+	write(1, "\n", 1);
+	ft_putnbr(ft_atoi_base("-ff", "0123456789abcdef"));
+	write(1, "\n", 1);
+	return (0);
+}
